ae16fd7121e7b4f161af7f2151a0daf28efcca39
AtlasGold <luan.vini31@hotmail.com>
2026-08-11 21:59:42 -0300

    Adopt existing catalog wrappers on update

--- ddl_original--1.0.0--1.0.1.sql
+++ ddl_original--1.0.0--1.0.1.sql
@@ -32,6 +32,39 @@ BEGIN
 END;
 $$;
 
+DO $$
+DECLARE
+    wrapper_oid oid;
+BEGIN
+    SELECT p.oid
+    INTO wrapper_oid
+    FROM pg_catalog.pg_proc AS p
+    JOIN pg_catalog.pg_namespace AS n
+      ON n.oid = p.pronamespace
+    JOIN pg_catalog.pg_language AS l
+      ON l.oid = p.prolang
+    WHERE n.nspname = 'pg_catalog'
+      AND p.proname = 'pg_get_functiondef'
+      AND pg_catalog.pg_get_function_identity_arguments(p.oid) = 'oid'
+      AND l.lanname = 'sql'
+      AND p.prosrc LIKE '%ddl_original.get_functiondef%';
+
+    IF wrapper_oid IS NOT NULL
+       AND NOT EXISTS (
+        SELECT 1
+        FROM pg_catalog.pg_depend AS d
+        JOIN pg_catalog.pg_extension AS e
+          ON e.oid = d.refobjid
+        WHERE d.classid = 'pg_catalog.pg_proc'::pg_catalog.regclass
+          AND d.objid = wrapper_oid
+          AND d.deptype = 'e'
+          AND e.extname = 'ddl_original'
+    ) THEN
+        EXECUTE 'ALTER EXTENSION ddl_original ADD FUNCTION pg_catalog.pg_get_functiondef(oid)';
+    END IF;
+END;
+$$;
+
 CREATE OR REPLACE FUNCTION pg_catalog.pg_get_functiondef(oid)
 RETURNS text
 LANGUAGE sql
@@ -70,6 +103,39 @@ BEGIN
         ELSE
             RAISE EXCEPTION 'Cannot find the native pg_catalog.pg_get_function_arguments(oid) to rename';
         END IF;
+    END IF;
+END;
+$$;
+
+DO $$
+DECLARE
+    wrapper_oid oid;
+BEGIN
+    SELECT p.oid
+    INTO wrapper_oid
+    FROM pg_catalog.pg_proc AS p
+    JOIN pg_catalog.pg_namespace AS n
+      ON n.oid = p.pronamespace
+    JOIN pg_catalog.pg_language AS l
+      ON l.oid = p.prolang
+    WHERE n.nspname = 'pg_catalog'
+      AND p.proname = 'pg_get_function_arguments'
+      AND pg_catalog.pg_get_function_identity_arguments(p.oid) = 'oid'
+      AND l.lanname = 'sql'
+      AND p.prosrc LIKE '%ddl_original.get_function_arguments%';
+
+    IF wrapper_oid IS NOT NULL
+       AND NOT EXISTS (
+        SELECT 1
+        FROM pg_catalog.pg_depend AS d
+        JOIN pg_catalog.pg_extension AS e
+          ON e.oid = d.refobjid
+        WHERE d.classid = 'pg_catalog.pg_proc'::pg_catalog.regclass
+          AND d.objid = wrapper_oid
+          AND d.deptype = 'e'
+          AND e.extname = 'ddl_original'
+    ) THEN
+        EXECUTE 'ALTER EXTENSION ddl_original ADD FUNCTION pg_catalog.pg_get_function_arguments(oid)';
     END IF;
 END;
 $$;
